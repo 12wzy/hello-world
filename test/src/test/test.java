0bb99b94582ae0b1562202a9b5479f591e86ae1a
Ziyang Wang <ziyang.wang@sap.com>
2016-08-05 10:05:23 +0800

    12333

--- test/src/test/test.java
+++ test/src/test/test.java
@@ -5,9 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello test");
-		System.out.println("push change to gerrit");
-		System.out.println("push change to gerrit");
-		System.out.println("push change to gerrit");
+		System.out.println("push change to github");
 	}
 
 }
